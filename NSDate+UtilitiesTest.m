8f755bb5295906c96afc2adda42f97f17b13b836
ZDima <mail@zdima.net>
2014-08-02 23:54:07 -0400

    added project file and test

---
+++ NSDate+UtilitiesTest.m
@@ -0,0 +1,61 @@
+//
+//  NSDate+UtilitiesTest.m
+//  NSDate+Utilities
+//
+//  Created by Dmitriy on 8/2/14.
+//  Copyright (c) 2014 ZDima. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+#import "NSDate+Utilities.h"
+#import "NSDate+Epoch.h"
+
+@interface NSDate_UtilitiesTest : XCTestCase
+@end
+
+@implementation NSDate_UtilitiesTest
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testExample {
+	
+	NSDate* date = [NSDate date];
+	NSInteger days = [date daysSinceEpoch];
+	
+	XCTAssert( [date isEqualToDate:[[date dateByAddingDays:110] dateBySubtractingDays:110]], "Add/Sub days");
+	
+	NSInteger distance = -[date distanceInDaysToDate:[NSDate dateWithTimeIntervalSince1970:0]];
+	
+	XCTAssert( [[date dateAtStartOfDay] isEqualToDate:[[[NSDate dateWithTimeIntervalSince1970:0] dateByAddingDays:distance] dateAtStartOfDay]], "distanceInDaysToDate/dateByAddingDays");
+	
+    XCTAssert( [[NSDate dateOnDay:days] isEqualTo:[date dateAtStartOfDay]] , @"[NSDate date] Pass");
+
+	date = [NSDate date];
+	const unsigned componentFlags = (NSYearCalendarUnit| NSMonthCalendarUnit | NSDayCalendarUnit | NSWeekCalendarUnit |  NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit | NSWeekdayCalendarUnit | NSWeekdayOrdinalCalendarUnit);
+	for( int n=0; n < 365; n++ ) {
+		for( int hour = 0; hour < 24; hour+=3 ) {
+			for( int minute = 0; minute < 60; minute+=3 ) {
+				for( int second = 0; second < 60; second+=21 ) {
+					NSDateComponents *components = [[NSDate currentCalendar] components:componentFlags fromDate:date];
+					components.hour = hour;
+					components.minute = minute;
+					components.second = second;
+					NSDate* td = [[NSDate currentCalendar] dateFromComponents:components];
+					days = [td daysSinceEpoch];
+					XCTAssert( [[NSDate dateOnDay:days] isEqualTo:[td dateAtStartOfDay]] , @"%@ Pass", td );
+				}
+			}
+		}
+		date = [date dateBySubtractingDays:1];
+	}
+}
+
+@end
